131428b3d2c5e17e60796e7ea4cd2560c23569a5
dungameo0204 <133087950+dungameo0204@users.noreply.github.com>
2026-01-01 01:13:07 +0700

    Them TURN server fix loi 4G

--- public/js/config.js
+++ public/js/config.js
@@ -1,9 +1,21 @@
+// public/js/config.js
 export const rtcConfig = {
     iceServers: [
+        // 1. STUN Server của Google (Giữ lại để dò đường cơ bản)
         { urls: 'stun:stun.l.google.com:19302' },
         { urls: 'stun:stun1.l.google.com:19302' },
-        { urls: 'stun:stun2.l.google.com:19302' },
-        { urls: 'stun:stun3.l.google.com:19302' },
-        { urls: 'stun:stun4.l.google.com:19302' },
-    ]
+        
+        // 2. TURN Server miễn phí của OpenRelayProject
+        // Giúp xuyên qua tường lửa 4G/5G khi STUN thất bại
+        {
+            urls: [
+                "turn:openrelay.metered.ca:80",
+                "turn:openrelay.metered.ca:443",
+                "turns:openrelay.metered.ca:443?transport=tcp"
+            ],
+            username: "openrelayproject",
+            credential: "openrelayproject"
+        }
+    ],
+    iceCandidatePoolSize: 10,
 };
